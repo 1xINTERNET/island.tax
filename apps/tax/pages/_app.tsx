fb88ea9976ab89a3f692418dad7b0d708c9ea5ad
mjakobs1x <83405126+mjakobs1x@users.noreply.github.com>
2025-05-08 11:41:47 +0200

    add head font face and styles for build to _app.tsx (#23)

--- apps/tax/pages/_app.tsx
+++ apps/tax/pages/_app.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react'
+
+import { fontStyles } from '../styles/font.css';
+
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 import { Header } from '@island.is/tax/components'
+import {PRELOADED_FONTS} from "@island.is/tax/constants";
 
 const Layout: FC<React.PropsWithChildren<unknown>> = ({ children }) => {
   const router = useRouter()
@@ -12,6 +16,20 @@ const Layout: FC<React.PropsWithChildren<unknown>> = ({ children }) => {
     <div>
       <Head>
         <title>Ísland.is</title>
+        {PRELOADED_FONTS.map((href, index) => {
+          return (
+            <link
+              key={index}
+              rel="preload"
+              href={href}
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
+            />
+          )
+
+        })}
+        <style>{fontStyles}</style>
       </Head>
       {!hideHeader && <Header megaMenuData={undefined} />}
       {children}
